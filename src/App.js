7d0f1867754c77ccc30559af49bc6414ba0bab66
sunip123 <sujit.singh@hotmail.com>
2020-08-05 22:54:17 +0530

    initial commit
    code for initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from '../src/components/UserList';
+import "./css/App.scss";
+
+
+function App() {
+  return (
+    <div className="App">
+    <UserList/>
+    </div>
+  );
+}
+
+export default App;
