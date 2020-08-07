c69e5bbde83438cc80225840aaadd749c882d9a8
SUJIT SINGH <sunip123@users.noreply.github.com>
2020-08-07 23:25:02 +0530

    added posts.js for the userposts

---
+++ src/components/Posts.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Search from '../components/Search'
+
+const Posts = ({ posts, loading }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="wrapper">
+      <Search/>
+        <div className="cards_wrap">
+          <div className="card_item">
+            <div className="card_inner">
+              {posts.map(post => (
+             <p key={post.id} className='list-group-item'>
+              <div className="card_top">
+                  <img src={post.avatar}  alt={post.first_name}/> 
+                </div>  
+              <div className="card_bottom">
+                 <div className="card_category">
+                  {post.first_name} {post.last_name}
+                   </div>
+                <div className="card_info">
+                  <p class="title"> {post.email}</p>
+                </div> 
+            </div>
+                </p>))} 
+             </div>
+             </div>
+         </div>
+     </div>
+  );
+};
+
+export default Posts;
